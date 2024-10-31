8394dd0a940b63b3ddac0404198368394153bbbd
Vennisha Naidoo <vennisha.naidoo@appstage.co.za>
2024-10-31 13:32:57 +0200

    Module.css to style component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import logo from '../assets/logo.png';
-import './Header.css';
+import classes from './Header.module.css';
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      {/* Using component.module.css to style - this makes the paragraph styling unique to this component */}
+      <p className={classes.paragraph}>A community of artists and art-lovers.</p>
     </header>
   );
 }
